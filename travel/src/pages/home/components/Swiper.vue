65978cc29a2f936c2ddfecc09e644ab9cbda7d46
RickHHH <1023006030@qq.com>
2021-03-24 16:27:53 +0800

    change

---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrapper">
+    <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" >
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default{
+    name:'HomeSwiper',
+    data (){
+        return{
+            swiperOption:{
+                pagination:'.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id:'0001',
+                imgUrl:'http://imgs.qunarzz.com/vs_ceph_vcimg/6ff121fd416169b8d56c60384e3baf79.jpeg'
+            },{
+                id:'0002',
+                imgUrl:'http://imgs.qunarzz.com/vs_ceph_vcimg/79faa5a73731e84a7731db49d5baa91e.jpeg'
+            }]
+        }
+    }
+}
+</script>
+
+
+
+<style lang="stylus" scoperd>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
+        overflow:hidden
+        width:100%
+        height:0
+        padding-bottom:31.25%
+        background-color #fff
+        .swiper-img
+            width:100%
+</style>
