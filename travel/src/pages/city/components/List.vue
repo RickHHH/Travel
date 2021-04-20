6dcec05f62204b42c9aaf2f871f02ba922270d9c
RickHHH <1023006030@qq.com>
2021-04-20 21:09:06 +0800

    1

--- travel/src/pages/city/components/List.vue
+++ travel/src/pages/city/components/List.vue
@@ -22,7 +22,7 @@
             </div>
             <div class="area" v-for="(item, key) of cities"
             :key="key">
-                <div class="title border-topbottom">{[key}}</div>
+                <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div 
                     class="item border-bottom"
